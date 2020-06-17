c52cc09e0c9c4dfe68bb5e5f2784488a1ce5b2bc
Martin Zuther <code@mzuther.de>
2020-06-18 00:08:58 +0200

    toggle energy saving

--- src/moccafaux/core.clj
+++ src/moccafaux/core.clj
@@ -18,8 +18,15 @@
    :dpms-enable  nil,
    :dpms-disable nil,
 
-   :suspend-enable  nil,
-   :suspend-disable nil })
+   :sleep-enable  nil,
+   :sleep-disable nil })
+
+
+;; force update on status
+(def status
+  (ref (zipmap [:processes :pulseaudio :zfs-scrub
+                :dpms-disabled :sleep-disabled]
+               (repeat nil))))
 
 
 (def settings
@@ -27,8 +34,8 @@
         user-settings  (try (json/read-str (slurp file-name)
                                            :key-fn keyword)
                             (catch Exception _
-                              (printf (format "Warning: could not open \"%s\"."
-                                              file-name))))
+                              (println (format "Warning: could not open \"%s\"."
+                                               file-name))))
         added-defaults (merge default-settings user-settings)]
     (->> added-defaults
          :monitor-processes-regex
@@ -37,31 +44,70 @@
          (assoc added-defaults :monitor-processes-cmd))))
 
 
-(defn shell-exec-success? [shell-cmd pred-key]
-  (when (settings pred-key)
-    (->> shell-cmd
-         (shell/sh "sh" "-c")
-         :exit
-         (= 0))))
+(defn shell-exec [command]
+  (->> command
+       (shell/sh "sh" "-c")
+       :exit))
+
+
+(defn shell-exec-success? [command pred-key]
+  (if (settings pred-key)
+    (->> (shell-exec command)
+         (= 0))
+    (println (format "Key %s not found in settings."
+                     pred-key))))
+
+
+(defn get-new-status []
+  (let [processes?  (shell-exec-success?
+                      (settings :monitor-processes-cmd)
+                      :monitor-processes)
+        pulseaudio? (shell-exec-success?
+                      "pactl list short sinks | grep -E '\\bRUNNING$'"
+                      :monitor-pulseaudio)
+        zfs-scrub?  (shell-exec-success?
+                      "zpool status | grep -E '(resilver|scrub) in progress'"
+                      :monitor-zfs-scrub)
+        new-status  {:processes      processes?
+                     :pulseaudio     pulseaudio?
+                     :zfs-scrub      zfs-scrub?
+                     :dpms-disabled  (boolean pulseaudio?)
+                     :sleep-disabled (boolean (or processes?
+                                                  pulseaudio?
+                                                  zfs-scrub?))}]
+    new-status))
+
+
+(defn update-power-management [selector]
+  (let [message {:dpms-enable   "Enabling DPMS ..... "
+                 :dpms-disable  "Disabling DPMS .... "
+                 :sleep-enable  "Enabling sleep .... "
+                 :sleep-disable "Disabling sleep ... "}
+        command (get settings selector)]
+    (when command
+      (print (message selector))
+      (if (shell-exec command)
+        (println "ok")
+        (println "failed")))))
 
 
 (defn update-status []
-  (let [processes?  (shell-exec-success?
-                     (settings :monitor-processes-cmd)
-                     :monitor-processes)
-        pulseaudio? (shell-exec-success?
-                     "pactl list short sinks | grep -E '\\bRUNNING$'"
-                     :monitor-pulseaudio)
-        zfs-scrub?  (shell-exec-success?
-                     "zpool status | grep -E '(resilver|scrub) in progress'"
-                     :monitor-zfs-scrub)
-        new-status  {:processes        processes?
-                     :pulseaudio       pulseaudio?
-                     :zfs-scrub        zfs-scrub?
-                     :dpms-disabled    (boolean pulseaudio?)
-                     :suspend-disabled (boolean (or processes?
-                                                    pulseaudio?
-                                                    zfs-scrub?))}]
+  (let [new-status     (get-new-status)
+        dpms-updated?  (not= (get @status :dpms-disabled)
+                             (get new-status :dpms-disabled))
+        sleep-updated? (not= (get @status :sleep-disabled)
+                             (get new-status :sleep-disabled))]
+    (dosync
+      (ref-set status
+               new-status))
+    (when sleep-updated?
+      (if (get @status :sleep-disabled)
+        (update-power-management :sleep-disable)
+        (update-power-management :sleep-enable)))
+    (when dpms-updated?
+      (if (get @status :dpms-disabled)
+        (update-power-management :dpms-disable)
+        (update-power-management :dpms-enable)))
     new-status))
 
 
@@ -71,4 +117,4 @@
                        (java.time.Duration/ofSeconds)
                        (chime/periodic-seq (java.time.Instant/now)))
                   (fn [_]
-                    (println (update-status)))))
+                    (update-status))))
