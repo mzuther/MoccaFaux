29a61eb726a62939807e07b4eaa34019e9fab7bb
Martin Zuther <code@mzuther.de>
2020-06-21 20:17:09 +0200

    re-factor (lightly)

--- src/de/mzuther/moccafaux/core.clj
+++ src/de/mzuther/moccafaux/core.clj
@@ -9,55 +9,22 @@
   (:gen-class))
 
 
-(defn- printfln [fmt & args]
-  (println (apply format fmt args)))
-
-
-(defn- print-header []
-  (let [fill-string  (fn [length char] (->> char
-                                            (repeat length)
-                                            (apply str)))
-        add-borders  (fn [s char] (string/join [char s char]))
-
-        header       (str "MoccaFaux v" (version/get-version "de.mzuther"
-                                                             "moccafaux.core"))
-
-        page-width   78
-        left-margin  (quot (- page-width (count header)) 2)
-        right-margin (- page-width (count header) left-margin)
-
-        full-line    (-> (fill-string page-width "-")
-                         (add-borders "o"))
-        full-header  (-> (string/join [(fill-string left-margin " ")
-                                       header
-                                       (fill-string right-margin " ")])
-                         (add-borders "|"))]
-    (println full-line)
-    (println full-header)
-    (println full-line)))
-
-
-(def default-preferences {:probing-interval 60,
-                          :watches          [],
-                          :tasks            {}})
-
-
-;; empty ref forces an update on first call to "update-status"
-(def task-states (ref {}))
-
-
 (def preferences
-  (try (let [file-name  (io/file (System/getProperty "user.home")
-                                 ".config" "moccafaux" "config.json")
-             user-prefs (json/read-str (slurp file-name)
-                                       :key-fn keyword)]
+  (try (let [;; "io/file" takes care of line separators
+             file-name     (io/file (System/getProperty "user.home")
+                                    ".config" "moccafaux" "config.json")
+             user-prefs    (json/read-str (slurp file-name)
+                                          :key-fn keyword)
+             default-prefs {:probing-interval 60,
+                            :watches          {},
+                            :tasks            {}}]
          (when-not (map? user-prefs)
            (throw (Exception. "JSON error (not handled by library)")))
-         (merge default-preferences
+         (merge default-prefs
                 user-prefs))
        (catch Throwable e
          (newline)
-         (println (.toString e))
+         (println (str e))
          (newline)
          (flush)
          (System/exit 1))))
@@ -67,6 +34,36 @@
   (->> preferences
        (sp/select [:tasks sp/MAP-KEYS])
        sort))
+
+
+;; empty ref forces an update on first call to "update-status"
+(def task-states (ref {}))
+
+
+(defn- printfln [fmt & args]
+  (println (apply format fmt args)))
+
+
+(defn- print-header []
+  (let [fill-string  (fn [length char] (->> char (repeat length) (apply str)))
+        add-borders  (fn [s char] (string/join [char s char]))
+
+        header       (str "MoccaFaux v" (version/get-version "de.mzuther" "moccafaux.core"))
+
+        page-width   78
+        header-width (count header)
+        left-margin  (quot (- page-width header-width) 2)
+        right-margin (- page-width header-width left-margin)
+
+        full-line    (add-borders (fill-string page-width "-") "o")
+        full-header  (add-borders
+                       (string/join [(fill-string left-margin " ")
+                                     header
+                                     (fill-string right-margin " ")])
+                       "|")]
+    (println full-line)
+    (println full-header)
+    (println full-line)))
 
 
 (defn- shell-exec [command fork?]
@@ -85,13 +82,15 @@
 
 
 (defn- status-shell-exec [[name {:keys [enabled command tasks]}]]
-  ;; enable energy saving when the shell command failed (returned a
-  ;; non-zero exit code), as this means that no interfering processes
-  ;; were found; for example, "grep" and "pgrep" return a non-zero
-  ;; exit code when they do not find matching lines or processes
+  ;; Enable energy saving when a task is enabled and the respective
+  ;; shell command failed (returned a *non-zero* exit code), as this
+  ;; means that no interfering processes (or whatever) were found.
+  ;;
+  ;; For example, "grep" and "pgrep" return a *non-zero* exit code
+  ;; when they do not find any matching lines or processes.
   (let [save-energy? (when enabled
-                       (-> (shell-exec command false)
-                           (= :failed)))]
+                       (= (shell-exec command false)
+                          :failed))]
     (reduce (fn [m k] (assoc m k (when (get tasks k)
                                    save-energy?)))
             {:name name}
